d87a3de9c60298cc01d749064ab5b02f262b6cc9
Enoch Abiodun <g00380316@atu.ie>
2023-10-12 21:23:58 +0100

    First iteration

---
+++ src/main/java/ie/atu/LibrayApp/Book.java
@@ -0,0 +1,17 @@
+package ie.atu.LibrayApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    private String title;
+    private String author;
+    private int isbn;
+    private int publishedYear;
+    
+}
---
+++ src/test/java/ie/atu/LibrayApp/LibrayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.LibrayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
